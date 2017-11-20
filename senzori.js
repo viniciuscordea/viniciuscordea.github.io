b411843fa4cbb405299cc19cc354cad48afd6945
viniciuscordea <viniciuscordea@gmail.com>
2017-11-20 18:27:26 +0200

    3

--- senzori.js
+++ senzori.js
@@ -1,21 +1,23 @@
+document.getElemntById("id_bussines_version").inerHTML="Businss version"
+
 window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
 //-----------------------------------------------------
 function on_device_orientation(e)
 {
-	document.getElementById("id_z").innerHTML = "z=" + e.alpha;
-	document.getElementById("id_x").innerHTML = "x=" + e.beta;
-	document.getElementById("id_y").innerHTML = "y=" + e.gamma;
+	document.getElementById("id_z").innerHTML = "z=" + Math.round(e.alpha*100)/100;
+	document.getElementById("id_x").innerHTML = "x=" + Math.round(e.beta*100)/100;
+	document.getElementById("id_y").innerHTML = "y=" + Math.round(e.gamma*100)/100;
 }
 //-----------------------------------------------------
 function on_device_motion(e)
 {
-	document.getElementById("id_acc_x").innerHTML = "acc_x=" + e.acceleration.x;
-	document.getElementById("id_acc_y").innerHTML = "acc_y=" + e.acceleration.y;
-	document.getElementById("id_acc_z").innerHTML = "acc_z=" + e.acceleration.z;
+	document.getElementById("id_acc_x").innerHTML = "acc_x=" + Math.round(e.acceleration.x*100)/100;
+	document.getElementById("id_acc_y").innerHTML = "acc_y=" + Math.round(e.acceleration.y*100)/100;
+	document.getElementById("id_acc_z").innerHTML = "acc_z=" + Math.round(e.acceleration.z*100)/100;
 
-	document.getElementById("id_acc_g_x").innerHTML = "acc_g_x=" + e.accelerationIncludingGravity.x;
-	document.getElementById("id_acc_g_y").innerHTML = "acc_g_y=" + e.accelerationIncludingGravity.y;
-	document.getElementById("id_acc_g_z").innerHTML = "acc_g_z=" + e.accelerationIncludingGravity.z;
+	document.getElementById("id_acc_g_x").innerHTML = "acc_g_x=" + Math.round(e.accelerationIncludingGravity.x*100)/100;
+	document.getElementById("id_acc_g_y").innerHTML = "acc_g_y=" + Math.round(e.accelerationIncludingGravity.y*100)/100;
+	document.getElementById("id_acc_g_z").innerHTML = "acc_g_z=" + Math.round(e.accelerationIncludingGravity.z*100)/100;
 }
 //-----------------------------------------------------
