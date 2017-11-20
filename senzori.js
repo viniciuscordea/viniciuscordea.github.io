ae4eccc3a04b68620d45b16825a47fa8a97557c3
viniciuscordea <viniciuscordea@gmail.com>
2017-11-20 19:04:37 +0200

    8

--- senzori.js
+++ senzori.js
@@ -21,8 +21,8 @@ function on_device_motion(e)
 	document.getElementById("id_acc_g_z").innerHTML = "acc_g_z=" + Math.round(e.accelerationIncludingGravity.z*100)/100;
 
 	
-	var beta=Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
-	var gamma=Math.atan(e.accelerationIncludingGravity.y/e.accelerationIncludingGravity.z)*180/Math.PI;
+	var beta=Math.atan(e.accelerationIncludingGravity.y/e.accelerationIncludingGravity.z)*180/Math.PI;
+	var gamma=Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
 
 	document.getElementById("id_rot_x").innerHTML = "beta=" + Math.round(beta*100)/100;
 	document.getElementById("id_rot_y").innerHTML = "gamma=" + Math.round(gamma*100)/100;
