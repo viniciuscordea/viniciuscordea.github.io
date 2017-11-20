4f7e329d7aef157844fc37f21bec756d5bb7ad9e
viniciuscordea <viniciuscordea@gmail.com>
2017-11-20 17:59:40 +0200

    2

--- senzori.js
+++ senzori.js
@@ -10,7 +10,12 @@ function on_device_orintation(e)
 
 function on_device_motion(e)
 {
-		
-	
-	
+	document.getElementById("id_acc_x").innerHTML="acc_x="e.acceleration.x;
+	document.getElementById("id_acc_y").innerHTML="acc_y="e.acceleration.y;
+	document.getElementById("id_acc_z").innerHTML="acc_z="e.acceleration.z;	
+
+	document.getElementById("id_acc_x").innerHTML="acc_g_x="e.accelerationIncludingGravity.x;
+	document.getElementById("id_acc_y").innerHTML="acc_g_y="e.accelerationIncludingGravity.y;
+	document.getElementById("id_acc_z").innerHTML="acc_g_z="e.accelerationIncludingGravity.z;
+
 }
