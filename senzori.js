b2731187e4f9620618d9447c01a0808761cceaf6
viniciuscordea <viniciuscordea@gmail.com>
2017-11-20 19:07:19 +0200

    10

--- senzori.js
+++ senzori.js
@@ -1,13 +1,13 @@
-document.getElementById("id_business_version").innerHTML="Business version=2017.11.20.1";
+document.getElementById("id_business_version").innerHTML="Business version=2017.11.20.2";
 
 window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
 //-----------------------------------------------------
 function on_device_orientation(e)
 {
-	document.getElementById("id_z").innerHTML = "z=" + Math.round(e.alpha*100)/100;
-	document.getElementById("id_x").innerHTML = "x=" + Math.round(e.beta*100)/100;
-	document.getElementById("id_y").innerHTML = "y=" + Math.round(e.gamma*100)/100;
+	document.getElementById("id_z").innerHTML = "alpha=" + Math.round(e.alpha*100)/100;
+	document.getElementById("id_x").innerHTML = "beta=" + Math.round(e.beta*100)/100;
+	document.getElementById("id_y").innerHTML = "gamma=" + Math.round(e.gamma*100)/100;
 }
 //-----------------------------------------------------
 function on_device_motion(e)
@@ -22,7 +22,7 @@ function on_device_motion(e)
 
 	
 	var beta=Math.atan(e.accelerationIncludingGravity.y/e.accelerationIncludingGravity.z)*180/Math.PI;
-	var gamma=Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
+	var gamma=-Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
 
 	document.getElementById("id_rot_x").innerHTML = "beta=" + Math.round(beta*100)/100;
 	document.getElementById("id_rot_y").innerHTML = "gamma=" + Math.round(gamma*100)/100;
