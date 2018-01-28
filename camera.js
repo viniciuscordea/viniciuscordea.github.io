43cf4f5f9a3f96cfb39f38056ecec5cf8bc3ea9a
viniciuscordea <viniciuscordea@gmail.com>
2018-01-28 15:05:37 +0200

    camera3

--- camera.js
+++ camera.js
@@ -1,6 +1,5 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
-//_______________________________________________###____________________________________________
 var constraints={audio: false,video: { facingMode: { exact: "environment" } }};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
@@ -8,17 +7,16 @@ video.addEventListener("touchstart",on_touch_video);
 video.addEventListener("mousedown",on_touch_video);
 
 
-//^^^^^^^^^^^^^^^^
 function on_cam(stream)
 {
 	video.srcObject=stream;
 }
-//^^^^^^^^^^^^^^^^6
+
 function on_error(e)
 {
 	alert("ERROR!: cannot connect to camera!");
 }
-//^^^^^^^^^^^^^^^^6
+
 function on_touch_video(e)
 {
 	var canvas = document.getElementById("id_img");
